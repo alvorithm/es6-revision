53960a12019678ce3f4d884a1962923a5f60a09b
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-03 17:06:58 +0200

    Went with template literals isnyead of concatenation

---
+++ 4.ES6_Template_Literals_and_String_Interpolation.js
@@ -0,0 +1,12 @@
+// function sayHi(name) {
+//     return "Hello there, " + name;
+// }
+
+// console.log(sayHi("Lemuel"));
+
+// Let us use template literals instead of uing concatenation
+function sayHi(name) {
+    return `Hello there, ${name}`;
+}
+
+console.log(sayHi("Lemuel"));
