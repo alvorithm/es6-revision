6cc6f792145493f06e54a12c3a35eda0cf2c2310
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-03 15:59:18 +0200

    const vs let

---
+++ 2.What_is_the_difference_between_var_let_and_const.js
@@ -0,0 +1,6 @@
+// cannot reassign new value to const variable onlye with let
+// const name = "Lemuel";
+//     name = Solomon;
+let myname = "Lemuel";
+    myname = "L Solomon";
+console.log(myname);
