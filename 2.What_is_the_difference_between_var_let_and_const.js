4dfea8d5d66c2ee23aa8694c3ee057e16e4e484e
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-03 16:13:14 +0200

    const and let and var differences cllearly illustrated

--- 2.What_is_the_difference_between_var_let_and_const.js
+++ 2.What_is_the_difference_between_var_let_and_const.js
@@ -14,10 +14,24 @@
 
 // //
 
-for (let i = 0; i < 3; i++) {
-    console.log(i);
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+// // with let keyword there will be a reference error: ReferenceError: i is not defined
+// // because the second cosnole.log method is declared outside the block scope where i 
+// // was initially declared
+// console.log("Outside", i);
+
+function checkPassword(password) {
+    const valid = password.length > 6;
+    if (valid) {
+        const message = "Your password is valid";
+        console.log(message);
+    } else {
+        const message = "Your password is not valid";
+        console.log(message);
+    }
 }
-// with let tkeyword there will be a rreference error: ReferenceError: i is not defined
-// because the second cosnole.log method is declared outside the block scope where i 
-// was initially declared
-console.log("Outside", i);
+
+checkPassword('3632');
+checkPassword('48278y87hkjsd');
