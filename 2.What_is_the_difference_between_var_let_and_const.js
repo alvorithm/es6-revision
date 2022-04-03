569ab174c1c1640e9c57a28720c41188fa352e2e
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-03 16:03:55 +0200

    with var i will still be pritned to the screen because var is not block scoped

--- 2.What_is_the_difference_between_var_let_and_const.js
+++ 2.What_is_the_difference_between_var_let_and_const.js
@@ -1,6 +1,13 @@
-// cannot reassign new value to const variable onlye with let
-// const name = "Lemuel";
-//     name = Solomon;
-let myname = "Lemuel";
-    myname = "L Solomon";
-console.log(myname);
+// // cannot reassign new value to const variable onlye with let
+// // const name = "Lemuel";
+// //     name = Solomon;
+// let myname = "Lemuel";
+//     myname = "L Solomon";
+// console.log(myname);
+
+
+for (var i = 0; i < 3; i++) {
+    console.log(i);
+}
+// with var i will still be pritned to the screen because var is not block scoped
+console.log("Outside", i);
