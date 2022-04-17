c6795914967dab098ba41f97c8356ca3288f6f39
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-17 10:11:45 +0200

    Basic greeting function before illustrating hoisting

--- 3.How_to_use_arrow_functions_in_JavaScript_ES6.js
+++ 3.How_to_use_arrow_functions_in_JavaScript_ES6.js
@@ -1,22 +1,33 @@
-// normal funcion using the function keyword
-function getInfo(name, age) {
-    return `Name: ${name}  - Age: ${age}`;
+// // normal funcion using the function keyword
+// function getInfo(name, age) {
+//     return `Name: ${name}  - Age: ${age}`;
+// }
+
+
+// console.log(getInfo("Alvorithm Ikiguy", 33));
+// // arrow function using the arrow syntax, const keyword  without the
+// // function keyword
+// const getInfo2 = (name, age) => {
+//     return `Name: ${name}  - Age: ${age}`;
+// };
+
+// console.log(getInfo2("Alvorithm Ikiguy", 33));
+
+// // arrow function: no need for return statement and curly parenthesis 
+// // when returning one value
+// const getInfo3 = (name, age) => `Name: ${name}  - Age: ${age}`;
+
+// console.log(getInfo3("Alvorithm Ikiguy", 33));
+
+// // no need for parenthesis if there is one argument being passed
+// const getInfo4 = name => `Name: ${name}`
+// console.log(getInfo4("Alvorithm Ikiguy", 33));
+
+// Function hositing, Variables and function initialisation logic, 
+// arrow function intiailisation and invoking logic
+
+function greeting() {
+    return "Hey there!";
 }
 
-
-console.log(getInfo("Alvorithm Ikiguy", 33));
-// arrow function using the arrow syntax, const keyword  without the fucntion keyword
-const getInfo2 = (name, age) => {
-    return `Name: ${name}  - Age: ${age}`;
-};
-
-console.log(getInfo2("Alvorithm Ikiguy", 33));
-
-// arrow function: no need for return statement and curly parenthesis when returning one value
-const getInfo3 = (name, age) => `Name: ${name}  - Age: ${age}`;
-
-console.log(getInfo3("Alvorithm Ikiguy", 33));
-
-// no need for parenthesis if there is one argument being passed
-const getInfo4 = (name) => `Name: ${name}`
-console.log(getInfo4("Alvorithm Ikiguy", 33));
+console.log(greeting());
