10e857e8aa1373e6990a77ad18329ef6505d97a1
Alvo Rithm <alvorithmemail@gmail.com>
2022-05-05 11:50:44 +0200

    fix(json file): had to add in type and module parameter to enable node-fetch import for succeessul api get request

--- 4.ES6_Template_Literals_and_String_Interpolation.js
+++ 4.ES6_Template_Literals_and_String_Interpolation.js
@@ -2,11 +2,34 @@
 //     return "Hello there, " + name;
 // }
 
-// console.log(sayHi("Alvorithm Ikiguy"));
+// console.log(sayHi("Lemuel Solomon"));
 
-// Let us use template literals instead of uing concatenation
-function sayHi(name) {
-    return `Hello there, ${name}`;
-}
+// Let us use template literals instead of using concatenation
+// function sayHi(name) {
+//     return `Hello there, ${name}`;
+// }
 
-console.log(sayHi("Alvorithm Ikiguy"));
+// console.log(sayHi("Lemuel Solomon"));
+
+// can use template literals to avoid typos with api calls - 
+// baseurl can be a variable and then it can be passed  into the fetch method
+// swapi is the star wars api
+
+// fetch API not implemented in node. Installed external module node-fecth
+// imported module at the top of code. pnpm added it autoatically to list of 
+// dependencies. Added in "type";
+
+import fetch from 'node-fetch';
+
+// using template literals to interpolate an abolsute link into the fetch 
+// method parameteric placeholders to avoid spelling mistakes
+
+const baseUrl = "https://swapi.dev/api/";
+
+fetch(`${baseUrl}people/1/`)
+   .then(res => res.json())
+   .then(json => console.log(json));
+
+fetch(`${baseUrl}people/2/`)
+   .then(res => res.json())
+   .then(json => console.log(json));
